94435923ea5868511c69fc233fd8f6b08104c6d4
Ramon de Paula Marques <ramonpm@gmail.com>
2019-03-04 17:52:15 -0300

    add graphql-yoga server + playground

--- handler.ts
+++ handler.ts
@@ -1,11 +1,7 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
-import { graphql } from 'graphql';
+import { GraphQLServerLambda } from 'graphql-yoga';
 import { schema } from './schema';
 
-export const queryProducts: APIGatewayProxyHandler = async (event, _context) => {
-  const result = await graphql(schema, event.body);
-  return {
-    statusCode: 200,
-    body: JSON.stringify(result),
-  };
-}
+const lambda = new GraphQLServerLambda({schema});
+
+export const queryProducts = lambda.graphqlHandler;
+export const playground = lambda.playgroundHandler;
